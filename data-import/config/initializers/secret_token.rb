ce60809dd1b0ba2fbb74c6a782760ac37e593707
Lauren Voswinkel <lvoswink@gmail.com>
2013-08-30 17:42:44 -0400

    Starting Rails project using Ruby 2.0/Rails 4.0

---
+++ data-import/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DataImport::Application.config.secret_key_base = 'bc6a55cfa75dff2638512a3dbff6ddac4eb96b85993b2b87bcc4d0acde001a1eb7f77a91d2fe585636cf9e085ab24b1e653641f2b60eb9001db705c1e7c98dec'
